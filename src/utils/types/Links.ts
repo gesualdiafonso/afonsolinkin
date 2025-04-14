3baeca906fdd83848b2d6e6715770f2824373778
Afonso Gesualdi <gesualdiafonsoarr@outlook.com>
2025-04-14 16:11:30 -0300

    "route config images"

--- src/utils/types/Links.ts
+++ src/utils/types/Links.ts
@@ -6,33 +6,34 @@ export interface LinkCardProp{
 
 export const Links: LinkCardProp[] = [
     {
-        title: "Porffólio",
+        title: "Portfólio",
         href: "https://devafonso.vercel.app",
-        image: "../public/assets/linkscapa/portfolio.png"
+        image: "/assets/linkscapa/portfolio.png"
     },
     {
         title: "Web Site",
         href: "https://afonsodev.vercel.app",
-        image: "../public/assets/linkscapa/site.png"
+        image: "/assets/linkscapa/site.png"
     },
     {
         title: "GitHub",
         href: "https://github.com/gesualdiafonso",
-        image: "../public/assets/linkscapa/github.png"
+        image: "/assets/linkscapa/github.png"
     },
     {
         title: "Behance",
         href: "https://www.behance.net/afonsogesualdi",
-        image: "../public/assets/linkscapa/behance.png"
+        image: "/assets/linkscapa/behance.png"
     },
     {
         title: "Anime Foto",
-        href: "none",
-        image: "../public/assets/foto_anime_afonso.jpeg"
+        href: "#",
+        image: "/assets/foto_anime_afonso.jpeg"
     },
     {
         title: "Spotify",
         href: "https://open.spotify.com/user/226clj4wl7buhdn5oi22464cq?si=4898b9c1f32248c2",
-        image: "../public/assets/linkscapa/spotify.png"
+        image: "/assets/linkscapa/spotify.png"
     },
 ]
+
